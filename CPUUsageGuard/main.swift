269704639dfc35f83112c7a4a0647d9f224c9275
Grigory Entin <grigory.entin@gmail.com>
2019-04-07 20:44:56 +0200

    Initial Commit

---
+++ CPUUsageGuard/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  CPUUsageGuard
+//
+//  Created by Grigory Entin on 07/04/2019.
+//  Copyright © 2019 Grigory Entin. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
